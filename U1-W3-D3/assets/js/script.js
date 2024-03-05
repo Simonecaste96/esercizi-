090d1c05bdb9a5383b1d7ed2a4a188e5538d4532
simonecaste96 <simonecaste96@gmail.com>
2024-03-05 22:33:04 +0100

    patch to do list, buttata giù qualche idea nuova

--- U1-W3-D3/assets/js/script.js
+++ U1-W3-D3/assets/js/script.js
@@ -1,58 +1,43 @@
 
-   /* const taskBtn = document.getElementById("taskBtn")
-    const pulisciBtn = document.getElementById("pulisciBtn")
-    const input = document.getElementById("newtask")
+const newTask = document.getElementById('newtask');
+const submit = document.getElementById('submit');
+const reset = document.getElementById('reset');
+const mission = document.getElementById('testo');
 
-    taskBtn.addEventListener("click", () => {
-        console.log("Task nuovo")
-
-    })
-
-    pulisciBtn.addEventListener("click", () => {
-        console.log("Task pulito")
-
-
-        
-    })
+newTask.addEventListener('keydown', (e)=>{
+if(e.key === 'Enter'){  
+submit.click();    
+}    
     
-    input.addEventListener("keypress", (element) => {
-      
-        
-    })
-*/
 
-// Ottieni riferimenti agli elementi del DOM
-const newTaskInput = document.getElementById('newtask');
-const addTaskButton = document.getElementById('taskBtn');
-const clearButton = document.getElementById('pulisciBtn');
-const todoList = document.getElementById('todolist');
+});
 
-// Aggiungi un gestore di eventi al click del bottone "Aggiungi Task"
-addTaskButton.addEventListener('click', function() {
-    // Ottieni il valore dell'input del nuovo task
-    const taskText = newTaskInput.value.trim();
+submit.addEventListener('click', (e)=>{
+ const testo = newTask.value;
     
     // Verifica se il campo del nuovo task è vuoto
-    if (taskText === '') {
-        alert('Inserisci un task valido.');
-        return;
+    if (testo) {
+     const newText = document.createElement('li');
+     newText.textContent = testo;
+    mission.appendChild(newText);
+    newTask.value = ""
     }
-    
-    // Crea un nuovo elemento di lista (li)
-    const newTaskItem = document.createElement('h4');
-    
-    // Aggiungi il testo del task all'elemento di lista
-    newTaskItem.textContent = taskText;
-    
-    // Aggiungi l'elemento di lista alla lista delle attività
-    todoList.appendChild(newTaskItem);
-    
-    // Pulisci il campo dell'input del nuovo task
-    newTaskInput.value = '';
+    else{
+    alert('Inserisci un task valido.');
+    } 
+
+     
 });
 
 // Aggiungi un gestore di eventi al click del bottone "Pulisci tutto"
-clearButton.addEventListener('click', function() {
+reset.addEventListener('click', (e) =>{
+mission.innerText = '';
+
     // Rimuovi tutti gli elementi di lista (li) dalla lista delle attività
-    todoList.innerHTML = '';
+    
 });
+
+mission.addEventListener('click', (e)=>{
+mission.style.textDecoration ='line-through';
+
+})
