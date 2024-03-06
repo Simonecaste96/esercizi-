0df04fac34e341622659a4da2f448a26fef2ec81
Simonecaste96 <simonecaste96@gmail.com>
2024-03-06 22:54:14 +0100

    terminata parte obbligatoria, a breve aggiungerò EXTRA

---
+++ U2-W3-D3/assets/js/script.js
@@ -0,0 +1,55 @@
+
+const add = document.getElementById('add');
+const del = document.getElementById('del');
+const row = document.getElementById('row');
+
+let arrayVuoto = [];
+
+
+
+const asin = async () => {
+    await fetch('https://striveschool-api.herokuapp.com/books')
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            arrayVuoto = data;
+            console.log(arrayVuoto);
+            popolazioneHTML();
+        })
+        .catch(err => {
+            console.log('Errore nel recupero dei dati: ', err);
+        });
+};
+
+
+
+function popolazioneHTML() {
+    for (let i = 0; i < arrayVuoto.length; i++) {
+    const div = document.createElement("div");
+    const cardAsin = arrayVuoto[i].asin;
+    const img = arrayVuoto[i].img;
+    const title = arrayVuoto[i].title;
+    const price = arrayVuoto[i].price;
+    const category = arrayVuoto[i].category;
+
+     div.classList.add('col');
+     div.innerHTML = `<div class="card" style="width: 18rem; height:40rem" id="${cardAsin}">
+     <img src="${img}" class="card-img-top" style="height:25rem" alt="Immagine libro :${title}">
+     <div class="card-body text-center">
+       <p class="card-title text-danger">${title}</p>
+       <p class="card-text">Price: ${price}€</p>
+       <p class="card-text">Category: ${category}</p>
+       <a class="btn btn-primary">Add to cart</a>
+     </div>
+   </div>`;
+   row.appendChild(div);   
+}
+
+}
+
+
+window.addEventListener('load', asin);
+
+
+
